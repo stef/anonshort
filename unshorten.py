1fb936af16b7af50781f7e1364325c5b88bcf0b1
stef <stefan.marsiske@gmail.com>
2012-01-18 02:56:44 +0100

    [doc] updated project name in license

--- unshorten.py
+++ unshorten.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# This file is part of unshorten.
+# This file is part of anonshort
 #
-#  unshorten is free software: you can redistribute it and/or modify
+#  anonshort is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Affero General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  unshorten is distributed in the hope that it will be useful,
+#  anonshort is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU Affero General Public License for more details.
 #
 #  You should have received a copy of the GNU Affero General Public License
-#  along with unshorten.  If not, see <http://www.gnu.org/licenses/>.
+#  along with anonshort  If not, see <http://www.gnu.org/licenses/>.
 #
 # (C) 2012- by Stefan Marsiske, <stefan.marsiske@gmail.com>
 
