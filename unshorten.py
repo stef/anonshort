d17f04a2255bc09b43bec68e6e583b05c3036ad7
stef <stefan.marsiske@gmail.com>
2012-01-18 03:32:16 +0100

    [enh] added unmeta - experimental

--- unshorten.py
+++ unshorten.py
@@ -19,10 +19,33 @@
 # (C) 2012- by Stefan Marsiske, <stefan.marsiske@gmail.com>
 
 
-import re
+import re, urllib2, cookielib, time
 from urlparse import urlparse, urlunparse
 from itertools import ifilterfalse
 import urllib, httplib
+from lxml.html.soupparser import parse
+
+opener=None
+#opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()),
+                              urllib2.ProxyHandler({'http': 'http://localhost:8123/'}))
+opener.addheaders = [('User-agent', 'parltrack/0.6')]
+def fetch(url, retries=5, ignore=[]):
+    if not opener:
+        init_opener()
+    # url to etree
+    try:
+        f=opener.open(url)
+    except (urllib2.HTTPError, urllib2.URLError), e:
+        if hasattr(e, 'code') and e.code>=400 and e.code not in [504, 502]+ignore:
+            logger.warn("[!] %d %s" % (e.code, url))
+            raise
+        if retries>0:
+            time.sleep(4*(6-retries))
+            f=fetch(url,retries-1, ignore=ignore)
+        else:
+            raise
+    return parse(f)
 
 utmRe=re.compile('utm_(source|medium|campaign|content)=')
 def urlSanitize(url, ua=None):
@@ -47,11 +70,21 @@ def urlSanitize(url, ua=None):
     tmp[4]='&'.join(ifilterfalse(utmRe.match, pcs.query.split('&')))
     return urlunparse(tmp)
 
+def unmeta(url):
+    for x in fetch(url).xpath('//meta[@http-equiv="refresh"]'):
+        newurl=x.get('content').split('; ')
+        if len(newurl)>1:
+            parts=urlparse(urllib.unquote_plus(newurl[1]))
+            if parts['scheme'] and parts['netloc'] and parts['path']:
+                url=newurl
+    return url
+
 def unshorten(url, ua=None):
     prev=None
     while url!=prev:
         prev=url
         url=urlSanitize(url,ua=ua)
+        url=unmeta(url)
     return url
 
 url="http://bit.ly/xJ5pK2"
