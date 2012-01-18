bd617cdb5df4e0a3d7ad87277c8bca968498935f
stef <stefan.marsiske@gmail.com>
2012-01-18 03:38:58 +0100

    [enh] refactored unmeta

--- unshorten.py
+++ unshorten.py
@@ -25,28 +25,6 @@ from itertools import ifilterfalse
 import urllib, httplib
 from lxml.html.soupparser import parse
 
-opener=None
-#opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()),
-                              urllib2.ProxyHandler({'http': 'http://localhost:8123/'}))
-opener.addheaders = [('User-agent', 'parltrack/0.6')]
-def fetch(url, retries=5, ignore=[]):
-    if not opener:
-        init_opener()
-    # url to etree
-    try:
-        f=opener.open(url)
-    except (urllib2.HTTPError, urllib2.URLError), e:
-        if hasattr(e, 'code') and e.code>=400 and e.code not in [504, 502]+ignore:
-            logger.warn("[!] %d %s" % (e.code, url))
-            raise
-        if retries>0:
-            time.sleep(4*(6-retries))
-            f=fetch(url,retries-1, ignore=ignore)
-        else:
-            raise
-    return parse(f)
-
 utmRe=re.compile('utm_(source|medium|campaign|content)=')
 def urlSanitize(url, ua=None):
     # handle any redirected urls from the feed, like
@@ -60,7 +38,7 @@ def urlSanitize(url, ua=None):
         req = urllib.quote(url[8+len(us.netloc):])
     #conn.set_debuglevel(9)
     headers={'User-Agent': ua or 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
-    conn.request("HEAD", req,None,headers)
+    conn.request("GET", req,None,headers)
     res = conn.getresponse()
     if res.status in [301, 304]:
         url = res.getheader('Location')
@@ -68,10 +46,10 @@ def urlSanitize(url, ua=None):
     pcs=urlparse(urllib.unquote_plus(url))
     tmp=list(pcs)
     tmp[4]='&'.join(ifilterfalse(utmRe.match, pcs.query.split('&')))
-    return urlunparse(tmp)
+    return (urlunparse(tmp), parse(res))
 
-def unmeta(url):
-    for x in fetch(url).xpath('//meta[@http-equiv="refresh"]'):
+def unmeta(url,root):
+    for x in root.xpath('//meta[@http-equiv="refresh"]'):
         newurl=x.get('content').split('; ')
         if len(newurl)>1:
             parts=urlparse(urllib.unquote_plus(newurl[1]))
@@ -83,8 +61,8 @@ def unshorten(url, ua=None):
     prev=None
     while url!=prev:
         prev=url
-        url=urlSanitize(url,ua=ua)
-        url=unmeta(url)
+        url,root=urlSanitize(url,ua=ua)
+        url=unmeta(url,root)
     return url
 
 url="http://bit.ly/xJ5pK2"
